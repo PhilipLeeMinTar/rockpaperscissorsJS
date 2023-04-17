1ffa1b25e0ee44fe3b84b7f5204b9586cb343cc5
PhilipLeeMinTar <philiplee98@gmail.com>
2023-04-18 01:13:12 +0800

    not pretty but a simple ui for this game

--- rps.js
+++ rps.js
@@ -5,36 +5,80 @@ let getComputerChoice = () => {
 
 let playRound = (playerSelection, computerSelection) => { 
     playerSelection = playerSelection.toLowerCase();
+    const text = document.createElement('div');
+    const div = document.querySelector('.result');
     if (playerSelection === 'rock'){
-        if (computerSelection === 'paper') {console.log("You lose, paper beats rock"); return -1;}
-        else if (computerSelection === 'scissors') {console.log("You win, rock beats scissors"); return 1;}
-        else {console.log("draw, both chose rock"); return 0;}
+        if (computerSelection === 'paper') {
+            text.innerText = "You lose, paper beats rock"; 
+            div.appendChild(text);
+            return -1;}
+        else if (computerSelection === 'scissors') {
+            text.innerText = "You win, rock beats scissors"; 
+            div.appendChild(text);
+            return 1;}
+        else {
+            text.innerText = "Draw, both chose rock"; 
+            div.appendChild(text);
+            return 0;}
     }else if (playerSelection === 'paper'){
-        if (computerSelection === 'rock') {console.log("You win, paper beats rock"); return 1;}
-        else if (computerSelection === 'scissors') {console.log("You lose, scissors beats paper"); return -1;}
-        else {console.log("draw, both chose paper"); return 0;}
+        if (computerSelection === 'rock') {
+            text.innerText = "You win, paper beats rock"; 
+            div.appendChild(text);
+            return 1;}
+        else if (computerSelection === 'scissors') {
+            text.innerText = "You lose, scissors beats paper"; 
+            div.appendChild(text);
+            return -1;}
+        else {
+            text.innerText = "Draw, both chose paper"; 
+            div.appendChild(text);
+            return 0;}
     }else if (playerSelection === 'scissors'){
-        if (computerSelection === 'paper') {console.log("You win, scissors beats paper"); return 1;}
-        else if (computerSelection === 'rock') {console.log("You lose, rock beats scissors"); return -1;}
-        else {console.log("draw, both chose scissors"); return 0;}
+        if (computerSelection === 'paper') {
+            text.innerText = "You win, scissors beats paper";
+            div.appendChild(text); 
+            return 1;}
+        else if (computerSelection === 'rock') {
+            text.innerText = "You lose, rock beats scissors"; 
+            div.appendChild(text);
+            return -1;}
+        else {
+            text.innerText = "Draw, both chose scissors";
+            div.appendChild(text); 
+            return 0;}
     }
 }
 
-let game = function() {
-    let score = 0;
-    for (let i=0; i<5; i++){
-        let playerSelection = prompt("Make your choice: ");
+
+let playerScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => {
+    button.addEventListener('click', ()=> {
+        let playerSelection = button.textContent;
         let computerSelection = getComputerChoice();
+        const div = document.querySelector('.result');
+        console.log(playerSelection);
+        console.log(computerSelection);
         let result = playRound(playerSelection, computerSelection);
-        if (result === 1) score++;
-    }
-    return score;
-}
-// console.log(getComputerChoice())
-// const playerSelection = "sCisSors";
-// const computerSelection = getComputerChoice();
-// console.log(playerSelection, computerSelection)
-// console.log(playRound(playerSelection, computerSelection));
-let score = game();
-if (score < 3) alert(`Your score is ${score}, Computer wins`)
-else alert(`Your score is ${score}, You win`)
+        if (result === 1) playerScore++;
+        else if (result === -1) computerScore++;
+        const playerScoreText = document.createElement('span');
+        const computerScoreText = document.createElement('span');
+        playerScoreText.innerText = `Player Score: ${playerScore}   `;
+        computerScoreText.innerText = `Computer Score: ${computerScore}`;
+        div.appendChild(playerScoreText);
+        div.appendChild(computerScoreText);
+        if (playerScore === 5) {
+            const win = document.createElement('p');
+            win.innerText = 'Player wins!'
+            div.appendChild(win);
+        } else if (computerScore === 5) {
+            const win = document.createElement('p');
+            win.innerText = 'Computer wins!'
+            div.appendChild(win);
+        }
+    });
+});
+
